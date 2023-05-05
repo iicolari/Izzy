9f008557974eed03b28f69b7e589cc0292db69cb
iicolari <102631658+iicolari@users.noreply.github.com>
2023-05-04 21:10:50 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,28 +3,36 @@ import { gsap } from "gsap";
 //QUIZ section 1
 
 
-// gsap.from(".red", {duration:1, x:0})
-// gsap.to(".red", {duration:2, y:0, delay:2})
+/* gsap.from(".red", {duration:1, x:0, ease:"power1.out"})
+gsap.to(".red", {duration:2, y:0, delay:2, ease:"power1.out"})
 
-// gsap.from(".blue", {duration:1, x:350})
-// gsap.to(".blue", {duration:2, y:300, delay:2})
+gsap.from(".blue", {duration:1, x:350, ease:"power1.out"})
+gsap.to(".blue", {duration:2, y:300, delay:2, ease:"power1.out"}) */
 
 
 //QUIZ section 2
 
-gsap.from(".graph", {duration:2, alpha:0})
-gsap.from(".green", {duration:3, scale:0})
+/* gsap.from(".graph", {duration:2, alpha:0})
+gsap.from(".green", {duration:5, scale:0, transformOrigin:"0px 0px"})
 
 
-gsap.from(".red", {duration:1, x:0})
-gsap.to(".red", {duration:2, y:0, delay:2})
+gsap.from(".red", {duration:1, x:0, ease:"power1.out"})
+gsap.to(".red", {duration:2, y:0, delay:2, ease:"power1.out"})
 
-gsap.from(".blue", {duration:1, x:350})
-gsap.to(".blue", {duration:2, y:300, delay:2})
+gsap.from(".blue", {duration:1, x:350, ease:"power1.out"})
+gsap.to(".blue", {duration:2, y:300, delay:2, ease:"power1.out"}) */
 
 //QUIZ section 3
 
+gsap.from(".graph", {duration:2, alpha:0})
+gsap.from(".green", {duration:6, scale:0, transformOrigin:"0px 0px"})
 
+
+gsap.from(".red", {duration:1, x:0, delay:2, ease:"bounce.out"})
+gsap.to(".red", {duration:2, y:0, delay:4, ease:"back.out", rotation:360})
+
+gsap.from(".blue", {duration:1, x:350, delay:2, ease:"bounce.out"})
+gsap.to(".blue", {duration:2, y:300, delay:4, ease:"back.out", rotation:360})
 
 
 //Comments
