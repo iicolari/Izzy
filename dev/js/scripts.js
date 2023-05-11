7757bb9f5b80be22ae6ac8f99c260237ebb690d4
iicolari <102631658+iicolari@users.noreply.github.com>
2023-05-10 22:23:38 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,16 +4,54 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 
+
+
+
+
+
+// gsap.utils.toArray(".section-1").forEach(section => {
+// 	let tl = gsap.timeline({
+// 			scrollTrigger: {
+// 				trigger: section,
+// 				start: "center center",
+//         // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+// 				end: () => "+=" + section.offsetWidth, 
+// 				scrub: true,
+// 				pin: true,
+//         anticipatePin: 1
+// 			},
+// 			defaults: {ease: "none"}
+// 		});
+// 	// animate the container one way...
+// 	tl.fromTo(section.querySelector("#section-1"), { xPercent: 100, x: 0}, {xPercent: 0})
+// 	  // ...and the image the opposite way (at the same time)
+// 	  .fromTo(section.querySelector("h1"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
+// });
+
+
+
+
+
+
+
+
 gsap.from("button", {
     scrollTrigger: {
         trigger: "button",
-        toggleActions: "restart pause resume restart"
+        toggleActions: "restart pause resume restart",
+        scrub: true
 },
     x: -200,
     width: 600,
     start: "20px 80%",
-    duration: 1.5
+    duration: .5
 });
+
+
+
+
+
+
 
 
 let delSections = document.querySelectorAll(".section-2");
@@ -66,21 +104,8 @@ delSections.forEach(section => {
 
 });
 
-gsap.utils.toArray(".comparisonSection").forEach(section => {
-	let tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: section,
-				start: "center center",
-        // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
-				end: () => "+=" + section.offsetWidth, 
-				scrub: true,
-				pin: true,
-        anticipatePin: 1
-			},
-			defaults: {ease: "none"}
-		});
-	// animate the container one way...
-	tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0}, {xPercent: 0})
-	  // ...and the image the opposite way (at the same time)
-	  .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
-});
+
+
+
+
+
