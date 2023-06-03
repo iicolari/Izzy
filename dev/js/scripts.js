0b6d38a15205e5b95cf19d37fb47f8f1d2480c0d
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-03 16:24:06 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,28 @@
 import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-import { MotionPathHelper } from "gsap/MotionPathHelper";
 
-gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin, MotionPathHelper);
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
 
+
+gsap.set("#mouth-1", {autoAlpha:0});
+gsap.set("#mouth-2", {autoAlpha:0});
+gsap.set("#mouth-3", {autoAlpha:0});
+gsap.set("#mouth-4", {autoAlpha:0});
+gsap.set("#mouth-5", {autoAlpha:0});
+
+
+function start(){
+    let tl = gsap.timeline();
+
+    tl.from("#dot-1", {duration:5, y:"-=500", ease:"none"})
+
+}
+
+
+let mainTl = gsap.timeline();
+mainTl.add(start)
+
+;
 
 GSDevTools.create();
