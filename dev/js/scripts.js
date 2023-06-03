8321661d98ddfab67e0bd8ef03b474590772d31f
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-03 14:21:02 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,11 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin, MotionPathHelper);
+
+
+GSDevTools.create();
