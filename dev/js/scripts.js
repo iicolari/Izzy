cadb94cc3476e296e1eb5a3df374a35dcbd52993
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-04 11:08:24 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,28 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
+
+
+function start(){
+    let tl = gsap.timeline();
+
+    tl.from("#1-wing", {
+        duration:1, 
+        scale:0,
+        rotate: 100});
+    }
+
+
+
+
+let mainTl = gsap.timeline();
+mainTl.add(start)
+
+
+;
+
+GSDevTools.create();
+
+
