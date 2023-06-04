83e661bcdbcee50958c71d1216776ac8bc343a11
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-03 23:02:27 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,25 +15,25 @@ function start(){
     let tl = gsap.timeline();
 
     tl.to("#dot-1, #dot-2, #dot-3", {
-        duration:.15, 
+        duration:.1, 
         stagger: .2,
         y: -10, 
         ease:"power4.out"})
 
         .to("#dot-1, #dot-2, #dot-3", {
-        duration:.15, 
+        duration:.1, 
         stagger: .2,
         y: 0, 
         ease:"power4.out"})
         
         .to("#dot-1, #dot-2, #dot-3", {
-        duration:.15, 
+        duration:.1, 
         stagger: .2,
         y: -10, 
         ease:"power4.out"})
 
         .to("#dot-1, #dot-2, #dot-3", {
-        duration:.15, 
+        duration:.1, 
         stagger: .2,
         y: 0, 
         ease:"power4.out"});
@@ -56,41 +56,58 @@ function transition(){
     let tl = gsap.timeline();
 
     tl.to("#dot-2", {
-        delay: 3,
+        delay: 2,
         duration:.5, 
         scale: 0,
         transformOrigin: "center"
     })
 
     .from("#mouth-1", {
-        delay: 3,
+        delay: 2,
         duration:.5, 
-        scale: 0,
+        scaleY: 0,
         transformOrigin: "center"
     }, 0)
 
     .to("#dot-1, #dot-3", {
-        delay: 3,
+        delay: 2,
         duration:.5, 
         y: -5
     }, 0)
 
+    .from("#antenna", {
+        delay: 2.25,
+        duration:.5, 
+        y: 20
+    }, 0)
+
+    .from("#left-ear", {
+        delay: 2.5,
+        duration:.5, 
+        x: 5
+    }, 0)
+
+    .from("#right-ear", {
+        delay: 2.75,
+        duration:.5, 
+        x: -5
+    }, 0)
+
     .to("#logo", {
-        delay: 2,
         duration:1, 
         transformOrigin: "bottom left",
         rotate: 0,
         //ScaleY: .5, 
-        ease:"power4.out"})
-
-
+        ease:"power4.out"}), 0
     }
 
-function mouth(){
+function final(){
     let tl = gsap.timeline();
 
-    tl.to("#mouth-2", {
-        
+   tl.from("#signal", {
+        delay: 3.5,
+        scale: 0,
+        transformOrigin: "center"
     })
 }
 
@@ -101,7 +118,7 @@ let mainTl = gsap.timeline();
 mainTl.add(start)
 .add(change, ">")
 .add(transition, ">")
-.add(mouth, ">")
+.add(final, ">")
 
 ;
 
