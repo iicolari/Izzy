e59a5e607808cee951d58828d704f60ca67f2568
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-03 22:18:23 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,12 +5,11 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
 
 
-gsap.set("#mouth-1", {autoAlpha:0});
 gsap.set("#mouth-2", {autoAlpha:0});
 gsap.set("#mouth-3", {autoAlpha:0});
 gsap.set("#mouth-4", {autoAlpha:0});
 gsap.set("#mouth-5", {autoAlpha:0});
-
+gsap.set("#logo", {scale:.75, transformOrigin:"center"});
 
 function start(){
     let tl = gsap.timeline();
@@ -18,20 +17,91 @@ function start(){
     tl.to("#dot-1, #dot-2, #dot-3", {
         duration:.15, 
         stagger: .2,
-        yoyo: true,
         y: -10, 
+        ease:"power4.out"})
+
+        .to("#dot-1, #dot-2, #dot-3", {
+        duration:.15, 
+        stagger: .2,
+        y: 0, 
+        ease:"power4.out"})
+        
+        .to("#dot-1, #dot-2, #dot-3", {
+        duration:.15, 
+        stagger: .2,
+        y: -10, 
+        ease:"power4.out"})
+
+        .to("#dot-1, #dot-2, #dot-3", {
+        duration:.15, 
+        stagger: .2,
+        y: 0, 
         ease:"power4.out"});
+    }
+
+
+function change(){
+    let tl = gsap.timeline();
+
+    tl.to("#logo", {
+        delay: 2,
+        duration:1, 
+        transformOrigin: "bottom left",
+        rotate: -10,
+        //ScaleY: .5, 
+        ease:"power4.out"})
+    }
+
+function transition(){
+    let tl = gsap.timeline();
+
+    tl.to("#dot-2", {
+        delay: 3,
+        duration:.5, 
+        scale: 0,
+        transformOrigin: "center"
+    })
+
+    .from("#mouth-1", {
+        delay: 3,
+        duration:.5, 
+        scale: 0,
+        transformOrigin: "center"
+    }, 0)
+
+    .to("#dot-1, #dot-3", {
+        delay: 3,
+        duration:.5, 
+        y: -5
+    }, 0)
+
+    .to("#logo", {
+        delay: 2,
+        duration:1, 
+        transformOrigin: "bottom left",
+        rotate: 0,
+        //ScaleY: .5, 
+        ease:"power4.out"})
 
 
     }
 
+function mouth(){
+    let tl = gsap.timeline();
 
+    tl.to("#mouth-2", {
+        
+    })
+}
 
 
 
 
 let mainTl = gsap.timeline();
 mainTl.add(start)
+.add(change, ">")
+.add(transition, ">")
+.add(mouth, ">")
 
 ;
 
