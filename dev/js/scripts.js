aaf82fbc0c0e9b7867d36157fe70f14f35372217
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-03 21:09:05 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,9 +15,19 @@ gsap.set("#mouth-5", {autoAlpha:0});
 function start(){
     let tl = gsap.timeline();
 
-    tl.from("#dot-1", {duration:5, y:"-=500", ease:"none"})
+    tl.to("#dot-1, #dot-2, #dot-3", {
+        duration:.15, 
+        stagger: .2,
+        yoyo: true,
+        y: -10, 
+        ease:"power4.out"});
 
-}
+
+    }
+
+
+
+
 
 
 let mainTl = gsap.timeline();
@@ -26,3 +36,14 @@ mainTl.add(start)
 ;
 
 GSDevTools.create();
+
+
+
+/* to("#dot-1", {
+        duration:0.15, 
+        scaleY: 0.8,
+        ease:"bounce.out"})
+
+        .to("#dot-1", {
+        duration:0.15})
+     */
