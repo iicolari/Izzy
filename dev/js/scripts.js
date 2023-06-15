dee77b0cdf2c542d02cbcf4261736a80e8031d2a
iicolari <102631658+iicolari@users.noreply.github.com>
2023-06-14 21:20:40 -0400

    save

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,36 +6,129 @@ gsap.registerPlugin(GSDevTools, MorphSVGPlugin);
 
 gsap.set("#logo", {scale:.75, transformOrigin:"center"});
 
-function start(){
-    let tl = gsap.timeline();
+function first(){
+    let tl = gsap.timeline({repeat: 1});
 
-    tl.to("#dot-1, #dot-2, #dot-3", {
+    tl.to("#dot-1", {
         duration:.1, 
-        stagger: .1,
-        y: -10, 
+        scaleY: 0.9,
+        transformOrigin: "bottom",
         ease:"power4.out"})
 
-        .to("#dot-1, #dot-2, #dot-3", {
-        delay: .1,
-        duration:.1, 
-        stagger: .1,
-        y: 0, 
+        .to("#dot-1", {
+        duration:.3, 
+        scaleY: 1.2,
+        y: -10, 
+        transformOrigin: "top",
         ease:"power4.out"})
         
-        .to("#dot-1, #dot-2, #dot-3", {
+        .to("#dot-1", {
         duration:.1, 
-        stagger: .1,
+        scaleY: 1,
         y: -10, 
+        transformOrigin: "top",
         ease:"power4.out"})
 
-        .to("#dot-1, #dot-2, #dot-3", {
-        duration:.1, 
-        stagger: .1,
+        .to("#dot-1", {
+        duration:.2, 
+        scaleY: 1.2,
         y: 0, 
-        ease:"power4.out"});
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+        .to("#dot-1", {
+        duration:.1, 
+        scaleY: 1,
+        y: 0, 
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
 
         return tl;
     }
+
+    function second(){
+    let tl = gsap.timeline({delay: .05, repeat: 1});
+
+    tl.to("#dot-2", {
+        duration:.1, 
+        scaleY: 0.9,
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+        .to("#dot-2", {
+        duration:.3, 
+        scaleY: 1.2,
+        y: -10, 
+        transformOrigin: "top",
+        ease:"power4.out"})
+        
+        .to("#dot-2", {
+        duration:.1, 
+        scaleY: 1,
+        y: -10, 
+        transformOrigin: "top",
+        ease:"power4.out"})
+
+        .to("#dot-2", {
+        duration:.2, 
+        scaleY: 1.2,
+        y: 0, 
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+        .to("#dot-2", {
+        duration:.1, 
+        scaleY: 1,
+        y: 0, 
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+
+        return tl;
+    }
+
+    function third(){
+    let tl = gsap.timeline({delay: .1, repeat: 1});
+
+    tl.to("#dot-3", {
+        duration:.1, 
+        scaleY: 0.9,
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+        .to("#dot-3", {
+        duration:.3, 
+        scaleY: 1.2,
+        y: -10, 
+        transformOrigin: "top",
+        ease:"power4.out"})
+        
+        .to("#dot-3", {
+        duration:.1, 
+        scaleY: 1,
+        y: -10, 
+        transformOrigin: "top",
+        ease:"power4.out"})
+
+        .to("#dot-3", {
+        duration:.2, 
+        scaleY: 1.2,
+        y: 0, 
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+        .to("#dot-3", {
+        duration:.1, 
+        scaleY: 1,
+        y: 0, 
+        transformOrigin: "bottom",
+        ease:"power4.out"})
+
+
+        return tl;
+    }
+
 
 
 function change(){
@@ -46,7 +139,7 @@ function change(){
         duration:.85, 
         transformOrigin: "bottom left",
         rotate: -10,
-        //scaleY: 0.9, 
+        //scaleY: 1.2,
         ease:"power4.out"})
 
         return tl;
@@ -122,7 +215,10 @@ function blink(){
         scaleY: 0,
         transformOrigin: "center"
         })
+
     }
+
+    
 
 
     function mouth(){
@@ -180,6 +276,125 @@ function blink(){
         return tl;
         }
 
+
+
+function changetwo(){
+    let tl = gsap.timeline({delay: 3.5});
+
+    tl.to("#logo", {
+        delay: 1.75,
+        duration:.85, 
+        transformOrigin: "bottom left",
+        rotate: -10,
+        //scaleY: 1.2,
+        ease:"power4.out"})
+
+        return tl;
+    }
+
+function transitiontwo(){
+    let tl = gsap.timeline({delay: 3.5});
+
+    tl.to("#dot-2", {
+        delay: 1.8,
+        duration:.25, 
+        scale: 1,
+        transformOrigin: "center"
+    })
+
+    .from("#mouth-1", {
+        delay: 1.8,
+        duration:.5, 
+        scaleY: 1,
+        transformOrigin: "center"
+    }, 0)
+
+    .to("#dot-1, #dot-3", {
+        delay: 1.8,
+        duration:.25, 
+        y: 0
+    }, 0)
+
+    .from("#antenna", {
+        delay: 1.9,
+        duration:.25, 
+        y: 0
+    }, 0)
+
+    .from("#left-ear", {
+        delay: 2,
+        duration:.25, 
+        x: 0
+    }, 0)
+
+    .from("#right-ear", {
+        delay: 2.1,
+        duration:.25, 
+        x: 0
+    }, 0)
+
+    .to("#logo", {
+        duration:1, 
+        transformOrigin: "bottom left",
+        rotate: 0,
+        //scaleY: 1, 
+        ease:"power4.out"}), 0
+
+        return tl;
+    }
+
+
+
+
+
+/* 
+function chat(){
+    let tl = gsap.timeline();
+
+   tl.to("#antenna", {
+        delay: 0,
+        y: 0
+   })
+
+   .to("#left-ear", {
+        delay: 0,
+        x: 0
+   })
+
+   .to("#right-ear", {
+        delay: 0,
+        x: 0
+   })
+
+    .to("#logo", {
+        duration: 1,
+        rotate: 0
+   })
+}
+ */
+
+
+
+let mainTl = gsap.timeline();
+mainTl.add(first)
+.add(second, ">")
+.add(third, ">")
+.add(change, ">")
+.add(transition, ">")
+.add(blink, ">")
+.add(mouth, ">")
+.add(changetwo, ">")
+.add(transitiontwo, ">")
+
+//.add(chat, ">")
+
+;
+
+GSDevTools.create();
+
+
+
+/* 
 function transitionback(){
     let tl = gsap.timeline();
 
@@ -216,56 +431,4 @@ function transitionback(){
         ease:"power4.out"
     })
 
-}
-
-/* 
-function chat(){
-    let tl = gsap.timeline();
-
-   tl.to("#antenna", {
-        delay: 0,
-        y: 0
-   })
-
-   .to("#left-ear", {
-        delay: 0,
-        x: 0
-   })
-
-   .to("#right-ear", {
-        delay: 0,
-        x: 0
-   })
-
-    .to("#logo", {
-        duration: 1,
-        rotate: 0
-   })
-}
- */
-
-
-
-let mainTl = gsap.timeline();
-mainTl.add(start)
-.add(change, ">")
-.add(transition, ">")
-.add(blink, ">")
-.add(mouth, ">")
-.add(transitionback, ">")
-//.add(chat, ">")
-
-;
-
-GSDevTools.create();
-
-
-
-/* to("#dot-1", {
-        duration:0.15, 
-        scaleY: 0.8,
-        ease:"bounce.out"})
-
-        .to("#dot-1", {
-        duration:0.15})
-     */
+} */
